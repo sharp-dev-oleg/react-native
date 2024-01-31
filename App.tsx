a3cd7553362f3091f4dd369782b3925495093968
Oleg Gordeev <oleg.gordeev@sharp-dev.net>
2024-01-31 13:04:17 +0300

    Add gluestack-ui

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-
 import {SafeAreaView, ScrollView, View} from 'react-native';
+
+import {
+  // AddIcon,
+  Button,
+  // ButtonIcon,
+  ButtonText,
+  GluestackUIProvider,
+  Text,
+} from '@gluestack-ui/themed';
+import {config} from '@gluestack-ui/config';
 
 import {Section} from './src/components/Section';
 import {Colors} from './src/colors';
@@ -18,18 +20,29 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: Colors.white,
-          }}>
-          <Section title="Initial">Initial</Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <GluestackUIProvider config={config}>
+      <SafeAreaView style={backgroundStyle}>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <View
+            style={{
+              backgroundColor: Colors.white,
+            }}>
+            <Section title="Initial">Initial</Section>
+            <Text>Test text</Text>
+            <Button
+              size="md"
+              variant="solid"
+              action="primary"
+              isDisabled={false}
+              isFocusVisible={false}>
+              <ButtonText>Add </ButtonText>
+            </Button>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GluestackUIProvider>
   );
 }
 
