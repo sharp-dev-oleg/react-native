68c4f7771671dfe2dcb6fbad02db51c7f56109da
Oleg Gordeev <oleg.gordeev@sharp-dev.net>
2024-02-08 13:47:58 +0300

    Save token to storage

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,12 +1,29 @@
-import {createContext, useContext, useState, PropsWithChildren} from 'react';
+import {
+  createContext,
+  useContext,
+  useState,
+  PropsWithChildren,
+  useCallback,
+} from 'react';
+import {MMKV} from 'react-native-mmkv';
 
 const TokenContext = createContext({
   token: '',
   setToken: (_token: string) => {},
 });
 
+export const storage = new MMKV();
+
 export const TokenProvider = ({children}: PropsWithChildren) => {
-  const [token, setToken] = useState('');
+  const [token, _setToken] = useState(() => storage.getString('token') ?? '');
+
+  const setToken = useCallback(
+    (token: string) => {
+      _setToken(token);
+      storage.set('token', token);
+    },
+    [_setToken],
+  );
 
   return (
     <TokenContext.Provider value={{token, setToken}}>
