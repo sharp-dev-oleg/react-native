1e34177876c66155af61720dde05df03603bbd04
Oleg Gordeev <oleg.gordeev@sharp-dev.net>
2024-02-21 15:20:56 +0300

    Autocomplete user names

--- src/screens/Send.tsx
+++ src/screens/Send.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {
   Alert,
   AlertIcon,
@@ -6,22 +6,28 @@ import {
   Button,
   ButtonText,
   Divider,
+  HStack,
   Heading,
   InfoIcon,
   Input,
   InputField,
   VStack,
+  BadgeText,
+  Badge,
+  Pressable,
 } from '@gluestack-ui/themed';
+import {debounce} from '../utils/debounce';
 import {Content} from '../components/Content';
 import {useUser} from '../hooks/useUser.tsx';
 import {createTransaction} from '../api/createTransaction.ts';
-import {getUsers} from '../api/getUsers.ts';
+import {getUsers, User} from '../api/getUsers.ts';
 
 export function Send(): React.JSX.Element {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
   const [error, setError] = useState('');
   const [success, setSuccess] = useState(false);
+  const [names, setNames] = useState<User[]>([]);
   const {token, refresh} = useUser();
   const onPress = useCallback(async () => {
     setError('');
@@ -36,14 +42,36 @@ export function Send(): React.JSX.Element {
     } catch (e) {
       setError((e as Error)?.message);
     }
-  }, [name, amount, token]);
+  }, [refresh, name, amount, token]);
 
-  useEffect(() => {
-    (async () => {
-      const users = await getUsers('oleg', token);
-      console.log('users', users);
-    })();
-  }, [token]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const findUsers = useCallback(
+    debounce(async (newName: string) => {
+      const users = await getUsers(newName, token);
+      setNames(users.slice(0, 4));
+    }, 500),
+    [],
+  );
+
+  const onNameChange = useCallback(
+    async (newName: string) => {
+      setName(newName);
+      if (newName.length <= 3) {
+        setNames([]);
+        return;
+      }
+
+      findUsers(newName);
+    },
+    [findUsers],
+  );
+
+  const onNameClick = useCallback(
+    (newName: string) => {
+      setName(newName);
+    },
+    [setName],
+  );
 
   return (
     <Content>
@@ -53,11 +81,25 @@ export function Send(): React.JSX.Element {
         <Input>
           <InputField
             value={name}
-            onChangeText={setName}
+            onChangeText={onNameChange}
             type="text"
             placeholder="Enter name"
           />
         </Input>
+        <HStack space="xs">
+          {names.map(user => (
+            <Pressable key={user.id} onPress={() => onNameClick(user.name)}>
+              <Badge
+                p="$0"
+                size="sm"
+                variant="solid"
+                borderRadius="$sm"
+                action="success">
+                <BadgeText>{user.name}</BadgeText>
+              </Badge>
+            </Pressable>
+          ))}
+        </HStack>
         <Input>
           <InputField
             value={amount}
